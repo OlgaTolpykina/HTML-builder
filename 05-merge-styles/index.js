8cd01082e1739699d707d459878259227bcb5aed
Olga Tolpykina <olga.belevantseva@gmail.com>
2021-11-04 16:17:12 +0300

    feat: add solution to task 5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const fs = require('fs');
+
+const pathToStyles = path.join(__dirname, 'styles');
+const pathToBundle = path.join(__dirname, 'project-dist', 'bundle.css');
+const bundle = fs.createWriteStream(pathToBundle);
+
+fs.readdir(pathToStyles, { withFileTypes:true }, (err, files) => {
+    files.forEach(file => {
+        if (file.isFile() && path.extname(file.name) == '.css') {
+            console.log(path.extname(file.name));
+            
+            const pathToFile = path.join(pathToStyles, file.name);
+            const readableStream = fs.createReadStream(pathToFile, 'utf-8');
+
+            let data='';
+            readableStream.on('data', chunk => bundle.write(data += chunk));
+        }
+    });
+});
